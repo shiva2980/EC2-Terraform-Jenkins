4ffbf95d95bce9b4ab9e7573c3932f97f394abed
shiva2980 <80401519+shiva2980@users.noreply.github.com>
2022-03-03 00:35:47 +0530

    Create VPC.tf

---
+++ VPC.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block       = " 0.0.0.0/32 "
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "mydemovpc"
+  }
+}
+resource "aws_subnet" "demosubnet-1" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "0.0.0.0/31"
+
+  tags = {
+    Name = "Subnet-1"
+  }
+}
+resource "aws_subnet" "demosubnet-2" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "0.0.0.0/30"
+
+  tags = {
+    Name = "Subnet-2"
+  }
+}
+resource "aws_internet_gateway" "mygw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "Myinternetgateway"
+  }
+}
+resource "aws_route_table" "myroute" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mygw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+resource "aws_route_table" "Private-RT" {
+  vpc_id = aws_vpc.myvpc.id
+  }
