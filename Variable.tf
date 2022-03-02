300e86744eca5ea31e393aa5e827b97765676469
shiva2980 <80401519+shiva2980@users.noreply.github.com>
2022-03-03 00:16:47 +0530

    Create Variable.tf

---
+++ Variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIATXZG5XN6V2R7SAX3"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+  default = "ap-south-1"
+}
